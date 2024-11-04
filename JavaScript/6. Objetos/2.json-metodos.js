d7615a06d839737243bc79c44442ba4f032e8873
Arturo Bernal <arturober@gmail.com>
2024-11-04 18:06:06 +0100

    objetos json

--- JavaScript/6. Objetos/2.json-metodos.js
+++ JavaScript/6. Objetos/2.json-metodos.js
@@ -11,3 +11,16 @@ function calculaDescuento(precio, descuento) {
 }
 
 console.log(producto.calculaDescuento(20));
+
+// Creamos el objeto como Object
+const producto2 = new Object(); // Creamos objeto genérico
+producto2.nombre = "Mesa";
+producto2.precio = 120;
+producto2.calculaDescuento = function(descuento) {
+    return this.precio * (100-descuento) / 100;
+}
+
+console.log(producto2.calculaDescuento(20));
+
+console.log(producto);
+console.log(producto2);
