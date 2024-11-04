7f6981d30f89a7d16c55c23cb7f6cd537161be58
Arturo Bernal <arturober@gmail.com>
2024-11-04 19:03:55 +0100

    array de objetos

--- JavaScript/Ejercicios/ejercicios de clase/6.objetos.js
+++ JavaScript/Ejercicios/ejercicios de clase/6.objetos.js
@@ -12,3 +12,29 @@ const rectangulo = {
 }
 
 console.log(rectangulo.getArea());
+
+/**
+ * Crea la clase Rectangulo que tendrá las propiedades ancho y alto, y 
+ * un método que se llame getArea y que devuelva el área del rectángulo
+ * Crea varios rectángulos a partir de la clase y muestra el área de cada 
+ * uno de ellos
+ */
+
+class Rectangulo {
+    constructor(ancho, alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    getArea() {
+        return this.alto * this.ancho;
+    }
+}
+
+const r1 = new Rectangulo(3, 6);
+const r2 = new Rectangulo(8, 12);
+const r3 = new Rectangulo(7, 13);
+
+console.log(r1.getArea());
+console.log(r2.getArea());
+console.log(r3.getArea());
---
+++ JavaScript/Ejercicios/ejercicios de clase/7.array_objetos.js
@@ -0,0 +1,29 @@
+class Producto {
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+
+  toString() {
+    return `${this.nombre}: ${this.precio}€`;
+  }
+}
+
+const productos = [
+  new Producto("Silla", 45),
+  new Producto("Mesa", 90),
+  new Producto("Armario", 230),
+  new Producto("Estantería", 132),
+  new Producto("Lámpara", 31),
+];
+
+console.log(productos.join(", "));
+productos.sort((p1, p2) => p1.precio - p2.precio);
+console.log(productos.join(", "));
+productos.sort((p1, p2) => p1.nombre.localeCompare(p2.nombre));
+console.log(productos.join(", "));
+
+// Productos precio < 100
+const prodMenor100 = productos.filter(p => p.precio < 100);
+console.log("Precio menor a 100: " + prodMenor100.join(", "));
+
