b180b5e5633e7abcdd15e3a145f5a7b1fc504779
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-11-15 20:20:50 +0100

    Se cambia icono del tema

--- HTML/formularios/google.js
+++ HTML/formularios/google.js
@@ -1,10 +1,14 @@
-document.getElementById("botonTema").addEventListener('click', () => {
-    const body = document.body;
+const body = document.body;
+const botonTema = document.getElementById("botonTema");
+const iconoTema = document.getElementById("iconoTema");
 
+botonTema.addEventListener('click', () => {
     if (body.getAttribute("data-bs-theme") === 'dark') {
         body.setAttribute('data-bs-theme', 'light');
+        iconoTema.classList.replace('bi-moon-stars-fill', 'bi-emoji-sunglasses-fill');
     }
     else {
         body.setAttribute('data-bs-theme', 'dark');
+        iconoTema.classList.replace('bi-emoji-sunglasses-fill', 'bi-moon-stars-fill');
     }
 });
