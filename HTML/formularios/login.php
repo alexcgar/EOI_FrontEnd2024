e76987c6688b523050335ddbaecaf1daae7b10ea
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-11-15 18:29:53 +0100

    Formulario de login

---
+++ HTML/formularios/login.php
@@ -0,0 +1,18 @@
+<?php
+
+    $usuario = $_POST['usuario'];
+    $contrasena = $_POST['usuario'];
+
+    if ($usuario == 'fernando' && $contrasena == '1234') {
+        echo "Eres Fernando, y acabas de hacer login";
+    }
+    else if ($usuario == 'alejandro' && $contrasena == 'realmadridelmejor') {
+        echo "Eres Alejandro y acabas de hacer login, y te gusta el Real Madrid";
+    }
+    else if ($usuario == 'leoncalavera2021@hotmal.com' && $contrasena == 'CaballoBruto') {
+        echo "Eres Juan Seoane";
+    }
+    else {
+        echo "Usuario o contraseña incorrectos";
+    }
+?>
