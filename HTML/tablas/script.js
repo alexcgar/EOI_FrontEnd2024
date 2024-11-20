e7da5458856eb3063233903afddcdc09110ca339
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-11-20 17:57:08 +0100

    Guardadas preferencias usuario en temas

--- HTML/tablas/script.js
+++ HTML/tablas/script.js
@@ -1,18 +1,34 @@
 document.getElementById('selector-claro-oscuro').addEventListener('change', function() {
-    document.body.setAttribute('data-bs-theme', this.value);
+    actualizaOpcionClaroOscuro(this.value);
 
     localStorage.setItem("opcion-claro-oscuro", this.value);
 });
 
 document.getElementById('selector-tema').addEventListener('change', function() {
-    const tema = this.value;
-    
+    actualizaTema(this.value);
+
+    localStorage.setItem("opcion-tema", this.value);
+});
+
+function actualizaOpcionClaroOscuro(opcion) {
+    document.body.setAttribute('data-bs-theme', opcion);
+}
+
+function actualizaTema(tema ) {
     if (tema == "bootstrap") {
         document.getElementById('enlace-tema').href = `https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css`;
     }
     else {
         document.getElementById('enlace-tema').href = `https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.3/${tema}/bootstrap.min.css`;
     }
+}
 
-    localStorage.setItem("opcion-tema", this.value);
-});
+function cargarAjustes() {
+    const opcion = localStorage.getItem("opcion-claro-oscuro");
+    if (opcion) actualizaOpcionClaroOscuro(opcion);
+
+    const tema = localStorage.getItem("opcion-tema");
+    if (tema) actualizaTema(tema);
+}
+
+cargarAjustes();
