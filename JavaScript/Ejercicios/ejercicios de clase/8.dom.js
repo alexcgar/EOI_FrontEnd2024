4c837107b55bc05d91c834d2a5631152c4801e0d
Arturo Bernal <arturober@gmail.com>
2024-11-22 18:39:20 +0100

    ejercicio DOM 2

---
+++ JavaScript/Ejercicios/ejercicios de clase/8.dom.js
@@ -0,0 +1,11 @@
+/** Cuando el usuario pase el ratón encima de la imagen, 
+ * esta cambiará a img/img2.webp. Al sacar el ratón, 
+ * volverá a la imagen anterior
+ */
+
+
+/**
+ * Cuando el usuario haga click al botón, cambia la clase
+ * btn-danger (rojo), por btn-success (verde). Cuando vuelva 
+ * a hacer click otra vez, vuelve al estado original
+ */
