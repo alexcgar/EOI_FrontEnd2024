7830e4ff5d931bc696f64acec1a14b1feb7a83b2
Arturo Bernal <arturober@gmail.com>
2024-11-22 19:04:15 +0100

    ejercicio 8 DOM

--- JavaScript/Ejercicios/ejercicios de clase/8.dom.js
+++ JavaScript/Ejercicios/ejercicios de clase/8.dom.js
@@ -3,9 +3,18 @@
  * volverá a la imagen anterior
  */
 
+const imagen = document.getElementById("imagen");
+imagen.addEventListener("mouseenter", e => imagen.src = "img/img2.webp");
+imagen.addEventListener("mouseleave", e => imagen.src = "img/img1.webp");
 
 /**
  * Cuando el usuario haga click al botón, cambia la clase
  * btn-danger (rojo), por btn-success (verde). Cuando vuelva 
  * a hacer click otra vez, vuelve al estado original
  */
+
+const boton = document.getElementById("boton");
+boton.addEventListener("click", e => {
+    boton.classList.toggle("btn-danger");
+    boton.classList.toggle("btn-success");
+});
