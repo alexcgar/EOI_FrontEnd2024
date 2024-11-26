fd6ba212a4c63b61df1dfbf43ab4957b4550b3b7
Arturo Bernal <arturober@gmail.com>
2024-11-26 20:36:55 +0100

    ejercicio fetch tareas

---
+++ JavaScript/Ejercicios/ejercicios de clase/10.fetch.js
@@ -0,0 +1,8 @@
+/**
+ * HAced petición GET con fetch a http://localhost:3000/tasks
+ * Recorred las tareas y para cada tarea hay que hacer lo siguiente:
+ * Crear un elemento li y meter dentro "id - description" de la tarea
+ * Podéis ver lo que devuelve el servidor en la pestaña de red
+ * de las herramientas de desarrollo del navegador
+ */
+
