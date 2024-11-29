62e13be9be7f9659981d23b2c0d9da97d9fe3ef0
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-11-29 20:42:13 +0100

    Nuevo componente

--- Angular/productos/src/app/products-page/products-page.component.ts
+++ Angular/productos/src/app/products-page/products-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'products-page',
+  standalone: true,
   imports: [],
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.css'
