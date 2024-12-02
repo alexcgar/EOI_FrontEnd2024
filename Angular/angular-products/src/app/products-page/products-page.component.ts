b50618daa5bf678fa91e5717f415d6d6e0d5f307
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-12-02 19:21:47 +0100

    Bucle de productos

--- Angular/angular-products/src/app/products-page/products-page.component.ts
+++ Angular/angular-products/src/app/products-page/products-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../interfaces/product';
 
 @Component({
   selector: 'products-page',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './products-page.component.css'
 })
 export class ProductsPageComponent {
-
+  title = "Mi lista de productos";
+  products: Product[] = [{
+    id: 1,
+    description: 'SSD hard drive',
+    available: '2019-10-03',
+    price: 80,
+    imageUrl: '/ssd.jpg',
+    rating: 5
+  }, {
+    id: 2,
+    description: 'LGA1212 Motherboard',
+    available: '2020-10-03',
+    price: 98,
+    imageUrl: '/motherboard.jpg',
+    rating: 4
+  }];
 }
