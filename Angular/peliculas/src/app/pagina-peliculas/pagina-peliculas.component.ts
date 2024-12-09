b3d8542621b54a4691126b0a2d33922cdb04c010
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-12-09 20:00:54 +0100

    Ya se muestra la primera película

--- Angular/peliculas/src/app/pagina-peliculas/pagina-peliculas.component.ts
+++ Angular/peliculas/src/app/pagina-peliculas/pagina-peliculas.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { PeliculasService } from '../services/peliculas.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'pagina-peliculas',
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './pagina-peliculas.component.css'
 })
 export class PaginaPeliculasComponent {
+  peliculasService = inject(PeliculasService);
+  peliculas = signal<any>([]);
 
+  constructor() {
+    this.obtenerChiste();
+  }
+
+  obtenerChiste() {
+    this.peliculasService
+    .getPeliculas()
+    .subscribe({
+      next: (peliculas) => this.peliculas.set(peliculas),
+      error: (error: HttpErrorResponse) => console.error(`Error obteniendo peliculas: `, error),
+    });
+  }
 }
