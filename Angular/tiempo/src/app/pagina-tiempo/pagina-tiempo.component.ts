eef540daecf72013a44b3a177fc6632c8c675131
fernando-ruiz-rico <fernando@iessanvicente.com>
2024-12-16 19:19:30 +0100

    Se muestra información del tiempo de Guarromán en el console.log

--- Angular/tiempo/src/app/pagina-tiempo/pagina-tiempo.component.ts
+++ Angular/tiempo/src/app/pagina-tiempo/pagina-tiempo.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { TiempoService } from '../services/tiempo.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'pagina-tiempo',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './pagina-tiempo.component.css'
 })
 export class PaginaTiempoComponent {
+  tiempoService = inject(TiempoService);
+  tiempo = signal<any>({});
 
+  constructor() {
+    this.obtenerTiempo();
+  }
+
+  obtenerTiempo() {
+    this.tiempoService
+    .getTiempo()
+    .subscribe({
+      next: (tiempo) => {
+        console.log(tiempo);
+        this.tiempo.set(tiempo)
+      },
+      error: (error: HttpErrorResponse) => console.error(`Error obteniendo pokemon: `, error),
+    });
+  }
 }
